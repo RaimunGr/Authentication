9a56edebcb92aa4f04308e4103c57aecc4f0cce0
Aerandir Tech <repository@aerandirtech.com>
2021-09-23 18:32:38 +0100

    Add user logics.

---
+++ 01. Core/Core.DomainModel/Data/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Core.DomainModel.Data
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+        User GetById(Guid id);
+        IEnumerable<User> GetAll();
+        User Remove(User user);
+    }
+}
--- 01. Core/Core.DomainModel/Entities/User.cs
+++ 01. Core/Core.DomainModel/Entities/User.cs
@@ -1,6 +1,30 @@
-﻿namespace Core.DomainModel.Entities
+﻿using System;
+
+namespace Core.DomainModel.Entities
 {
     public sealed class User
     {
+        private User()
+        { }
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public User Create(string firstName, string lastName, string username, string password)
+        {
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Password = password,
+                CreationDate = DateTime.UtcNow,
+            };
+        }
     }
 }
